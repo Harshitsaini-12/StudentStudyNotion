e779f0cfa432f9d8fdd87b410e282593788d1a8c
Harshitsaini-12 <harshitsaini3582@gmail.com>
2024-05-03 17:09:55 +0530

    backend final changes

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -135,8 +135,8 @@ exports.verifySignature = async (req, res) => {
                 //mail send krdo confirmation wala 
                 const emailResponse = await mailSender(
                                         enrolledStudent.email,
-                                        "Congratulations from StudyBuddy",
-                                        "Congratulations, you are onboarded into new StudyBuddy Course",
+                                        "Congratulations from CodeHelp",
+                                        "Congratulations, you are onboarded into new CodeHelp Course",
                 );
 
                 console.log(emailResponse);
